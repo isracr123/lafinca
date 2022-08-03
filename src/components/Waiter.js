10fa1d3fea4329305fcd29cec466dc0d1d4445ab
isracr123 <a01328862@itesm.mx>
2022-08-02 22:51:13 -0500

    waiter

--- src/components/Waiter.js
+++ src/components/Waiter.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Moment from 'react-moment'
 import {useState, useEffect} from 'react'
-import { PlusIcon } from '@heroicons/react/solid'
+import { PlusIcon, MinusIcon } from '@heroicons/react/solid'
 import axios from 'axios'
+import Autocomplete from 'react-autocomplete';
+console.warn = () => {}
 
 const EditTicket = () => {
 
@@ -15,7 +17,7 @@ const EditTicket = () => {
   /* Variables*/
   const [name, setName] = useState();
   const [table, setTable] = useState();
-  const [qty, setQty] = useState();
+  const [qty, setQty] = useState(1);
   const [products, setProducts] = useState();
   const [price, setPrice] = useState();
   const [subtotal, setSubtotal] = useState();
@@ -24,15 +26,39 @@ const EditTicket = () => {
   const [change, setChange] = useState();
 
   /*Filter */
-  const [searchBarFilter, setSearchBarFilter] = useState('');
-  const [focus, setFocus] = useState(false);
-  const [product, setProduct] = useState('');
+  const [value, setValue] = useState('');
 
+  /*Inicio*/
+  const [start, setStart] = useState(false);
+ 
   /*Get Products*/
   const getProductsData = async () => {
     const p = await axios.get('http://localhost:4000/api/products');
     setProducts(p.data);
   } 
+
+  /*Filter price */
+  const filterPrice = (product) => {
+    const array = products.filter((item) => item.product.toLowerCase().includes(product.toLowerCase()));
+    setPrice(array[0].price);
+    setSubtotal(array[0].price*qty)
+  }
+
+  /*Sum products*/
+  const sumProducts = () => {
+    setQty(qty+1);
+    setSubtotal(price*(qty+1));
+  }
+
+  /*Rest Products*/
+  const restProducts = () => {
+    if (qty > 0) {
+        setQty(qty-1);
+        setSubtotal(price*(qty-1));
+    }else{
+
+    }
+  }
 
   return (
     <div className='fixed w-full h-screen left-0 top-0 z-10 flex flex-wrap justify-center content-center p-1 overflow-y-scroll'>
@@ -75,7 +101,17 @@ const EditTicket = () => {
                                     <tbody>
                                         <tr>
                                             <td className='py-1 text-center flex-col'>
-                                                <input className='w-5 border-2 border-black' onChange={event => setQty(event.target.value)} defaultValue={1}/>
+                                                <button className="absolute left-3 md:left-96 bg-rose-500 text-white active:bg-rose-600 font-bold uppercase text-xs px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                onClick={() => {sumProducts()}}>
+                                                    <PlusIcon className="h-2 w-2 text-white"/>
+                                                </button>
+                                                <button className="absolute top-[320px] left-3 md:left-96 md:top-[368px] bg-rose-500 text-white active:bg-rose-600 font-bold uppercase text-xs px-2 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                onClick={() => {restProducts()}}>
+                                                    <MinusIcon className="h-2 w-2 text-white"/>
+                                                </button>
+                                                <p className='w-5 border-2 border-black'>
+                                                    {qty}
+                                                </p>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -86,30 +122,25 @@ const EditTicket = () => {
                                     <tbody>
                                         <tr>
                                             <td className='py-1 text-left'>
-                                                <input className='w-13 border-2 border-black' defaultValue={product} onChange={(e)=>setSearchBarFilter(e.target.value)} 
-                                                onFocus={(e) => {
-                                                  setFocus(true);
-                                                }}/>
-                                                {focus ?
-                                                  <div className="z-10 w-30 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
-                                                    <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
-                                                    {products && products.filter((p)=>{
-                                                      if(p.product.toLowerCase().includes(searchBarFilter.toLowerCase())){
-                                                        return p;
-                                                      }
-                                                    }).map(p =>
-                                                    <li key={p._id}>
-                                                        <button className="w-32 block py-1 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
-                                                        onClick={() => {
-                                                          const function1 = setProduct(p.product);
-                                                          /* const function2 = setFocus(false); */
-                                                        }}>
-                                                          {p.product}
-                                                        </button>
-                                                    </li>)}
-                                                    </ul>
-                                                  </div>
-                                                : null}
+                                                {start ? 
+                                                    <Autocomplete
+                                                        getItemValue={(item) => item.product}
+                                                        items={products.filter((item) => item.product.toLowerCase().includes(value.toLowerCase()))}
+                                                        renderItem={(item, isHighlighted) =>
+                                                            <div key={item.product} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                                                            {item.product}
+                                                            </div>
+                                                        }
+                                                        wrapperStyle={{
+                                                            border: "solid 2px #000000"
+                                                        }}
+                                                        value={value || ''}
+                                                        onChange={(e) => setValue(e.target.value)}
+                                                        onSelect={(value) => {
+                                                            const function1 = setValue(value);
+                                                            const function2 = filterPrice(value);
+                                                        }}
+                                                    />: null}
                                             </td>
                                         </tr>
                                     </tbody>
@@ -122,7 +153,9 @@ const EditTicket = () => {
                                             <td className='py-1 text-center'>
                                                 <div className='grid grid-cols-2 space-x-2'>
                                                     <p>$</p> 
-                                                    <input className='w-7 border-2 border-black' onChange={event => setPrice(event.target.value)}/>
+                                                    <p className='w-7 border-2 border-black'>
+                                                        {price}
+                                                    </p>
                                                 </div>
                                             </td>
                                         </tr>
@@ -136,7 +169,9 @@ const EditTicket = () => {
                                             <td className='py-1 text-center'>
                                                 <div className='grid grid-cols-2 space-x-2'>
                                                     <p>$</p> 
-                                                    <input className='w-7 border-2 border-black' onChange={event => setSubtotal(event.target.value)}/>
+                                                    <p className='w-7 border-2 border-black'>
+                                                        {subtotal}
+                                                    </p>
                                                 </div>
                                             </td>
                                         </tr>
@@ -147,54 +182,19 @@ const EditTicket = () => {
                     </tbody> 
                 </table>
             </div>
-            <div className='relative left-1/2 top-1'>
-              <button type="button" className=" text-green-700 border border-green-700 hover:bg-green-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:focus:ring-green-800">
+            <div className='relative left-72 top-1'>
+              <button type="button" className=" text-green-700 border border-green-700 hover:bg-green-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:focus:ring-green-800"
+              onClick={() => setStart(true)}>
                   <PlusIcon className="h-5 w-5 text-green-500"/>
               </button>
             </div>
             <div className='w-full border-t border-gray-300 my-2'></div>
-            <div>
-            <div className='flex font-semibold'>
-                <div className='flex-grow'>TOTAL</div>
-                <div className='grid grid-cols-2'>
-                    <p className='px-5'>$</p>
-                    <input className='w-12 border-2 border-black' onChange={event => setTotal(event.target.value)} defaultValue="2"/>
-                </div>
-            </div>
-            <div className='flex text-xs font-semibold'>
-                <div className='flex-grow'>Pago</div>
-                <div className='grid grid-cols-2'>
-                    <p className='px-5'>$</p>
-                    <input className='w-12 border-2 border-black' onChange={event => setPay(event.target.value)} defaultValue="2"/>
-                </div>
-            </div>
-            <div className='w-full border-t border-gray-300 my-2'></div>
-            <div className='flex text-xs font-semibold'>
-                <div className='flex-grow'>Cambio</div>
-                <div className='grid grid-cols-2'>
-                    <p className='px-5'>$</p>
-                    <input className='w-12 border-2 border-black' onChange={event => setChange(event.target.value)} defaultValue="2"/>
-                </div>
-            </div>
+            <div className='text-center'>
+                <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                    ENVIAR
+                </button>
             </div>
         </div>
-        </div>
-        <div className='p-4 w-full'>
-            {/* <button className='w-full text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700' onClick={()=>{
-            const function1 = window.location.href='http://localhost:3000/pos/tickets';
-            }}>
-            X Cerrar
-            </button>
-            <button className='w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'
-            onClick={()=>{
-                const function1 = updateTickets();
-            }}>
-                Actualizar
-            </button>
-            <button className='bg-yellow-500 text-white text-lg px-4 py-3 rounded-2xl w-full focus:outline-none'
-            onClick={handlePrint}>
-                IMPRIMIR
-            </button> */}
         </div>
     </div>
 </div>
